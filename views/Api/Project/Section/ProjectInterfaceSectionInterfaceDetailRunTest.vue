c82e95da5e1316ab0854e52d3d0a1fe84206d633
ShoneSingLone <ShoneSingLone@gmail.com>
2026-01-20 15:31:28 +0800

    ``` feat: 更新HTML模板和国际化配置
    - 优化cloud-disk.html和entry.yapi.html页面结构，简化HTML声明和meta标签
    - 移除不必要的换行和空格，压缩HTML代码
    - 更新JavaScript资源版本号为当前时间戳格式
    - 修正i18n/en-US.js中的国际化键名错误，将__success/__failed更正为xxx_success/xxx_failed
    - 添加新的表单验证和输入提示相关的国际化词条
    ```

--- views/Api/Project/Section/ProjectInterfaceSectionInterfaceDetailRunTest.vue
+++ views/Api/Project/Section/ProjectInterfaceSectionInterfaceDetailRunTest.vue
@@ -69,7 +69,7 @@
 </template>
 <script lang="ts">
 export default async function () {
-	debugger;
+	
 	return defineComponent({
 		inject: ["APP", "inject_project", "inject_interface_section_interface_detail"],
 		props: {
